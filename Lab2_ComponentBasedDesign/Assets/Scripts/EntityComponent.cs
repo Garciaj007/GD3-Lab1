96e55b930f15c8afc68ba689195c1c365b972c30
Noah <ncracknell.1@gmail.com>
2019-10-01 20:40:08 -0400

    New Scriptables. EntityComponent. New Prefabs.

---
+++ Lab2_ComponentBasedDesign/Assets/Scripts/EntityComponent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnPlayer", order = 1)]
+class Entity : ScriptableObject {
+    [Header("Attributes")]
+    public string name;
+    public Sprite sprite;
+    public float movementSpeed;
+    public float attackRate;
+    public float attackDamage;
+    public float MaxHealth;
+
+    [Header("SFX")]
+    public float rotationAmount;
+    public AnimationCurve curve;
+}
+
+[RequireComponent(typeof(HealthComponent))]
+[RequireComponent(typeof(SpriteRenderer))]
+class EntityComponent : MonoBehaviour {
+
+    [SerializeField] private Entity sciptable = null;
+    private SpriteRenderer sp = null;
+    private HealthComponent healthComponent = null;
+
+    private void Awake() {
+        sp = GetComponent<SpriteRenderer>();
+        healthComponent = GetComponent<HealthComponent>();
+    }
+
+    private void Start() {
+        sp.sprite = sciptable.sprite;
+        name = sciptable.name;
+        healthComponent.SetMaxHealth(sciptable.MaxHealth);
+    }
+
+    private void Update() {
+
+    }
+}
