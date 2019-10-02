b1ac10efddf615c484ec3774fd6d710438f23c58
Garciaj007 <jurielgarcia2010@gmail.com>
2019-10-02 01:23:34 -0400

    Render Quad & Render Texture Resizing

---
+++ Lab2_ComponentBasedDesign/Assets/Scripts/MotionBlurCapture.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MotionBlurCapture : MonoBehaviour
+{
+    [SerializeField] private Camera renderCamera;
+
+    private void Awake()
+    {
+        if (renderCamera.targetTexture != null)
+            renderCamera.targetTexture.Release();
+
+        renderCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 10);
+    }
+}
