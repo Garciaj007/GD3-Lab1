4444d257b92f23e9db0b1fe25a1f9c98eb72c48c
Garciaj007 <jurielgarcia2010@gmail.com>
2019-10-01 23:16:07 -0400

    Created Class to Attach Entity Sprite to Sprite Renderer

---
+++ Lab2_ComponentBasedDesign/Assets/Scripts/ApplyEntitySprite.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ApplyEntitySprite : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = 
+            GetComponent<EntityComponent>().Entity.sprite;
+    }
+}
