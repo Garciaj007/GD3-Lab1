6ace7229c9f70c6bcbb425185a6dbadf80546e28
Noah <ncracknell.1@gmail.com>
2019-10-02 01:20:19 -0400

    no message

---
+++ Lab2_ComponentBasedDesign/Assets/Scripts/CharacterClickController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent (typeof(PolygonCollider2D))]
+public class CharacterClickController : MonoBehaviour
+{
+
+    public delegate void OnCharacterClickDelegate(GameObject gameObject_);
+    public static event OnCharacterClickDelegate onCharacterClick;
+
+
+    void OnMouseDown()
+    {
+        if (onCharacterClick != null) onCharacterClick(this.gameObject);
+    }
+
+
+}
---
+++ Lab2_ComponentBasedDesign/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject currentSelected = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterClickController.onCharacterClick += ChangeCurrentSelected;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeCurrentSelected(GameObject gameObject_)
+    {
+        currentSelected = gameObject_;
+        Debug.Log("Selected: " + currentSelected.name);
+    }
+}
