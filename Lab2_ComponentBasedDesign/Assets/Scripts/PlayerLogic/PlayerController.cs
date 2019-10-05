bd3bc5edd47df33097161a81c6e015e72214c98d
Jaymie Xu <jake9828@gmail.com>
2019-10-04 21:08:16 -0400

    Added basic enemy detection

---
+++ Lab2_ComponentBasedDesign/Assets/Scripts/PlayerLogic/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public delegate void Trigger2DEnterDelegate(Collider2D info);
+    public event Trigger2DEnterDelegate OnTrigger2DEnter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        OnTrigger2DEnter?.Invoke(col);
+    }
+}
---
+++ Lab2_ComponentBasedDesign/Assets/Scripts/PlayerLogic/PlayerIdleState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : StateMachineBehaviour
+{
+    private PlayerController controller;
+
+    public override void OnStateEnter(UnityEngine.Animator animator, UnityEngine.AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        controller = animator.gameObject.GetComponent<PlayerController>();
+        controller.OnTrigger2DEnter += OnPlayerTrigger2DEnter;
+    }
+
+    public override void OnStateExit(UnityEngine.Animator animator, UnityEngine.AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        controller.OnTrigger2DEnter -= OnPlayerTrigger2DEnter;
+    }
+
+    private void OnPlayerTrigger2DEnter(Collider2D info)
+    {
+        if (info.tag == "EnemyUnit")
+        {
+            Debug.Log("Enemy unit detected!");
+        }
+    }
+}
