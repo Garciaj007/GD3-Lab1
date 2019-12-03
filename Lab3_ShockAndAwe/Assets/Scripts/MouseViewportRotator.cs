7ed396b0c6e8c10ef7a92a0d71aa865fdd595a9d
Juriel Garcia <jurielgarcia2010@gmail.com>
2019-12-02 22:51:07 -0500

    Minor Changes
    Changes to the Mouse Rotator Script

--- Lab3_ShockAndAwe/Assets/Scripts/MouseViewportRotator.cs
+++ Lab3_ShockAndAwe/Assets/Scripts/MouseViewportRotator.cs
@@ -8,29 +8,13 @@ public class MouseViewportRotator : MonoBehaviour
     void Update()
     {
         Camera cam = Camera.main;
-        Vector3[] frustumCorners = new Vector3[4];   
-        cam.CalculateFrustumCorners(new Rect(0,0,1,1), cam.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
+        Vector3[] frustumCorners = new Vector3[4];
 
-         for (int i = 0; i < 4; i++)
-        {
-            var worldSpaceCorner = cam.transform.TransformVector(frustumCorners[i]);
-            Debug.DrawRay(cam.transform.position, worldSpaceCorner, Color.blue);
-        }
-
-        cam.CalculateFrustumCorners(new Rect(0, 0, 1, 1), cam.farClipPlane, Camera.MonoOrStereoscopicEye.Left, frustumCorners);
+        cam.CalculateFrustumCorners(new Rect(0, 0, 1, 1), cam.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
 
         for (int i = 0; i < 4; i++)
         {
-            var worldSpaceCorner = cam.transform.TransformVector(frustumCorners[i]);
-            Debug.DrawRay(cam.transform.position, worldSpaceCorner, Color.green);
-        }
-
-        cam.CalculateFrustumCorners(new Rect(0, 0, 1, 1), cam.farClipPlane, Camera.MonoOrStereoscopicEye.Right, frustumCorners);
-
-        for (int i = 0; i < 4; i++)
-        {
-            var worldSpaceCorner = cam.transform.TransformVector(frustumCorners[i]);
-            Debug.DrawRay(cam.transform.position, worldSpaceCorner, Color.red);
+            Debug.DrawRay(cam.transform.position, frustumCorners[i], Color.red);
         }
     }
 }
